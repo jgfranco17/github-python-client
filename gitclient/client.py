2031dac6bd81622073393375ce111b110e3aa85c
Joaquin Franco <chino.franco@gmail.com>
2023-12-10 09:50:48 +0900

    feat: created authenticated request method

--- gitclient/client.py
+++ gitclient/client.py
@@ -5,30 +5,42 @@ from typing import Any, Dict
 
 import requests
 
-from .errors import ClientDataFetchError
+from .errors import ClientDataFetchError, ClientRequestError
 
 
 class GithubClient:
-    def __init__(self, user: str) -> None:
+    def __init__(self, user: str, version: str = "2022-11-28") -> None:
         self.__url = "https://api.github.com"
         self.__user = user
         self.__token = os.getenv("GITHUB_TOKEN")
+        self.__api_version = version
 
-    def __make_request(self, method, endpoint, params=None, data=None, headers=None):
+    def __make_base_request(
+        self, method, endpoint, params=None, data=None, headers=None
+    ) -> requests.Response:
         url = f"{self.__url}/{endpoint}"
         response = requests.request(
             method, url, params=params, data=data, headers=headers
         )
+        return response
 
-        if response.status_code == HTTPStatus.OK:
-            return response.json()
+    def __make_authenticated_request(
+        self, method: str, endpoint: str
+    ) -> Dict[str, Any]:
+        headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {self.__token}",
+            "X-GitHub-Api-Version": self.__api_version,
+        }
 
-        else:
-            response.raise_for_status()
+        response = self.__make_base_request(
+            method=method.upper(), endpoint=endpoint, headers=headers
+        )
+        return response.json()
 
     def get_user_info(self) -> Dict[str, Any]:
         endpoint = f"users/{self.__user}"
-        return self.__make_request("GET", endpoint)
+        return self.__make_base_request("GET", endpoint)
 
     def print_user_info(self) -> None:
         try:
@@ -43,4 +55,4 @@ class GithubClient:
 
     def get_repo_info(self, owner, repo) -> Dict[str, Any]:
         endpoint = f"repos/{owner}/{repo}"
-        return self.__make_request("GET", endpoint)
+        return self.__make_base_request("GET", endpoint)
--- gitclient/errors.py
+++ gitclient/errors.py
@@ -3,6 +3,11 @@ class GithubClientError(BaseException):
         super().__init__(f"[{identifier}] {message}")
 
 
+class ClientRequestError(GithubClientError):
+    def __init__(self, message: str):
+        super().__init__(identifier="ClientRequestError", message=message)
+
+
 class ClientDataFetchError(GithubClientError):
     def __init__(self, message: str):
         super().__init__(identifier="ClientFetchError", message=message)
